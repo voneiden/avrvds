066672d71b6efb4a85cba2cf661246835160507f
Matti Eiden <matti.eiden@bitfactor.fi>
2021-06-24 23:37:06 +0300

    Placeholder file

--- src/Views/ChipView.elm
+++ src/Views/ChipView.elm
@@ -1,12 +1,38 @@
 module Views.ChipView exposing (..)
 -- MODEL
 import Decoders.ChipDefinitionDecoder exposing (ChipDefinition)
-type alias AppState =
+import Main exposing (Session)
+
+
+type alias State =
     { chipDefinition : ChipDefinition
     , pinout : Int
     }
 
 type Model
-  = Failure
-  | Loading
-  | Success AppState
+  = Failure Session
+  | Loading Session
+  | Success Session State
+
+
+view : Model -> Html Msg
+view model =
+       case model of
+         Failure _ ->
+           div []
+             [ text "I could not load a random cat for some reason. "
+             , button [ onClick RequestDefinition ] [ text "Try Again!" ]
+             ]
+
+         Loading _ ->
+           text "Loading..."
+
+         Success _ appState ->
+           div []
+             [ button [ onClick RequestDefinition, style "display" "block" ] [ text "More Please!" ]
+             , div [] [
+                 case get appState.pinout appState.chipDefinition.pinouts of
+                     Nothing -> text "Nada"
+                     Just pinout -> text pinout.name
+                 ]
+             ]
