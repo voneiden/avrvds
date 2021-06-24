d260cf29f2566b362d13c987bd71cd8163cc6457
Matti Eiden <matti.eiden@bitfactor.fi>
2021-06-24 23:35:34 +0300

    Fix elm watch for subdirs

--- elm-watch.sh
+++ elm-watch.sh
@@ -1,4 +1,4 @@
 while true; do
-    ls -d src/*.elm | entr -d elm make src/Main.elm --output=app/avrvds.js;
+    find src/ -name "*.elm" | entr -d elm make src/Main.elm --output=app/avrvds.js;
     sleep 0.5;
 done
